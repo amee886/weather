77cb75cae41f48a1f1a779e6a6ae10a8354ee065
amee886 <nsecex@gmail.com>
2025-03-23 17:14:10 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
 import requests
 
-place_first= "London"
+
+place_first = "London"
 place_second = "svo"
-place_third= "Череповец"
-url_template="https://wttr.in/{}?nTqM&lang=ru"
+place_third = "Череповец"
+url_template = "https://wttr.in/{}?nTqM&lang=ru"
 url = url_template.format(place_third)
-response=requests.get(url)
+response = requests.get(url)
 response.raise_for_status()
 print(response.text)
-
