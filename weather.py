ddb701e01ae2af04ff497bae13c127192a985150
amee886 <nsecex@gmail.com>
2025-03-24 21:38:48 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,11 +1,16 @@
 import requests
 
 
-place_first = "London"
-place_second = "svo"
-place_third = "Череповец"
-url_template = "https://wttr.in/{}?nTqM&lang=ru"
-url = url_template.format(place_third)
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+place = ["London", "svo", "Череповец"]
+
+
+for i in place:
+    url_template = "https://wttr.in/"
+    url_first = url_template.format(i)
+    params = {
+        'nTqM': '',
+        'lang': 'ru'
+    }
+    response = requests.get(url_template + i, params=params)
+    response.raise_for_status()
+    print(response.text)
