b8b17f45e3e65b3d05c9eef82613371d818b217a
amee886 <nsecex@gmail.com>
2025-04-11 18:20:20 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,16 +1,14 @@
 import requests
 
 
-place = ["London", "svo", "Череповец"]
+locations = ["London", "svo", "Череповец"]
 
 
-for i in place:
-    url_template = "https://wttr.in/"
-    url_first = url_template.format(i)
+for i in locations:
     params = {
         'nTqM': '',
         'lang': 'ru'
     }
-    response = requests.get(url_template + i, params=params)
+    response = requests.get(f"https://wttr.in/{i}",params=params)
     response.raise_for_status()
     print(response.text)
