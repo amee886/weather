ab24f58d9fce16a6039b06078c131c56d010ce3e
amee886 <nsecex@gmail.com>
2025-04-16 23:03:56 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,17 +1,30 @@
 import requests
 
 
+def create_params():
+    return {
+        'nTqM': '',
+        'lang': 'ru'
+    }
+
+
+def fetch_weather(location, params):
+    response = requests.get(f"https://wttr.in/{location}", params=params)
+    response.raise_for_status()
+    return response.text
+
+
+def print_weather(location):
+    params = create_params()
+    weather_info = fetch_weather(location, params)
+    print(weather_info)
+
+
 def get_weather(locations):
     for location in locations:
-        params = {
-            'nTqM': '',
-            'lang': 'ru'
-        }
-        response = requests.get(f"https://wttr.in/{location}", params=params)
-        response.raise_for_status()
-        print(response.text)
+        print_weather(location)
 
 
 if __name__ == "__main__":
     locations = ["London", "svo", "Череповец"]
-    get_weather(locations)  
+    get_weather(locations)
